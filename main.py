c23e7c7cbb0ede572646ff5612e4c336192c106b
Joel Schutz <joeymarley@gmail.com>
2020-06-04 20:48:38 -0300

    Limpando o arquivo main de lixo

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 #Importa métodos
 import ficha
 import meta
-from appJar import gui
 
-p1 = meta.load_obj('bob','personagem')
+p1 = ficha.personagem()
 
-print(p1.dados())
-
-# app = gui("Ficha 3D&T", "400x350")
-
-# app.addLabel("title", "Ficha do Personagem")
-# app.addScrolledTextArea("t1", text=p1.print_ficha())
-
-# app.go()
-
-# p1 = ficha.personagem()
-
-# ficha.criar(p1, 'bob', 1, 17)
+ficha.criar(p1, 'bob', 1, 17)
 
 # meta.save_obj(p1)
 
